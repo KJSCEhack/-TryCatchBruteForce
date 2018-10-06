02f7b78de4536baa28c4cb380191f839badcdd5f
Humaira <sayyed.humaira@gmail.com>
2018-10-06 11:54:35 +0530

    python files merged

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask import Flask, render_template, flash, redirect, url_for, session, req
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+import MySQLdb# I was using it like this
 from functools import wraps
-
+import math
+import json
 app = Flask(__name__)
 
 # Config MySQL
@@ -17,8 +19,17 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
 #Articles = Articles()
-
+# import necessary packages
+ 
+try:
+    CONN   = MySQLdb.connect(host = 'localhost', user = 'root', passwd = 'password', db = 'kjcse')
+    CURSOR = CONN.cursor()
+    CURSOR = CONN.cursor(MySQLdb.cursors.DictCursor) 
+except Exception as e:
+    print(e)
+    exit()
 # Index
+
 @app.route('/')
 def index():
     return render_template('home.html')
@@ -44,29 +55,38 @@ def student_data():#navbar student ka hoga in card layout
 def student_track():
     cur = mysql.connection.cursor()
     data = {}
+    s1 = []
+    s2 = []
+    s3 = []
     # Get articles8
     #result = cur.execute("SELECT * FROM student")
     if(cur.execute("SELECT s1 from student where login = %s", session["username"])):
-        data['s1'] = cur.execute("SELECT attendance, tot_att FROM s1 where student_id = %s", session["username"])
-        data['s1'] = cur.fetchall()
-        if data['s1'][0]/data['s1'][1] * 100 > 70:
-            data['s1'] = str(70/100* data['s1'][1]/data['s1'][0])
+        s1 = cur.execute("SELECT attendance, tot_att FROM s1 where student_id = %s", session["username"])
+        s1 = cur.fetchall()
+        print(s1)
+        if int(s1[0]['attendance'])/int(s1[0]['tot_att']) * 100 < 70:
+            data['s1'] = str(math.ceil(70/100* int(s1[0]['tot_att'])/int(s1[0]['attendance'])))
         else:
             data['s1'] = "safe"
     if(cur.execute("SELECT s2 from student where login = %s", session["username"])):
-        data['s2'] = cur.execute("SELECT attendance, tot_att FROM s2 where student_id = %s", session["username"])
-        data['s2'] = cur.fetchall()
-        if data['s2'][0]/data['s2'][1] * 100 > 70:
-            data['s2'] = str(70/100* data['s2'][1]/data['s2'][0])
+        s2 = cur.execute("SELECT attendance, tot_att FROM s2 where student_id = %s", session["username"])
+        s2 = cur.fetchall()
+        print(s2)
+        print(int(s2[0]['attendance'])/int(s2[0]['tot_att']) *100)
+        if int(s2[0]['attendance'])/int(s2[0]['tot_att']) * 100 < 70:
+            data['s2'] = str(math.ceil(70/100* int(s2[0]['tot_att'])/int(s2[0]['attendance'])))
         else:
             data['s2'] = "safe"
     if(cur.execute("SELECT s3 from student where login = %s", session["username"])):
-        data['s3'] = cur.execute("SELECT attendance, tot_att FROM s3 where login = %s", session["username"])
-        data['s3'] = cur.fetchall()
-        if data['s3'][0]/data['s3'][1] * 100 > 70:
-            data['s3'] =str( 70/100* data['s3'][1]/data['s3'][0])
+        s3 = cur.execute("SELECT attendance, tot_att FROM s3 where student_id = %s", session["username"])
+        s3 = cur.fetchall()
+        print(s3)
+        if int(s3[0]['attendance'])/int(s3[0]['tot_att']) * 100 < 70:
+            data['s3'] = str(math.ceil(70/100* int(s3[0]['tot_att'])/int(s3[0]['attendance'])))
         else:
-            data['s2'] = "safe"
+            data['s3'] = "safe"
+
+        print(data)
     
 
     
@@ -229,13 +249,19 @@ def article(id):
 # Register Form Class
 class RegisterForm(Form):
     name = StringField('Name', [validators.Length(min=1, max=50)])
-    username = StringField('Username', [validators.Length(min=4, max=25)])
+    username = StringField('UserID', [validators.Length(min=4, max=25)])
     email = StringField('Email', [validators.Length(min=6, max=50)])
     password = PasswordField('Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords do not match')
     ])
+
     confirm = PasswordField('Confirm Password')
+    class1 = StringField('Class 1')
+    class2 = StringField('Class 2')
+    class3 = StringField('Class 3')
+    c1 = StringField('Committee Notifications 1')
+    c2 = StringField('Committee Notifications 2')
 
 
 # User Register
@@ -246,13 +272,18 @@ def register():
         name = form.name.data
         email = form.email.data
         username = form.username.data
-        password = sha256_crypt.encrypt(str(form.password.data))
+        password = form.password.data
+        class1 = form.class1.data
+        class2 = form.class2.data
+        class3 = form.class3.data
+        c1 = form.c1.data 
+        c2 = form.c2.data
 
         # Create cursor
         cur = mysql.connection.cursor()
 
         # Execute query
-        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+        cur.execute("INSERT INTO student(login, password, s1, s2, s3,c1,c2) VALUES(%s, %s, %s, %s, %s, %s, %s)", (username, password, class1, class3, class3, c1,c2))
 
         # Commit to DB
         mysql.connection.commit()
@@ -295,7 +326,7 @@ def login():
                 session['username'] = username
 
                 flash('You are now logged in', 'success')
-                return redirect(url_for('dashboard'))
+                return redirect(url_for('student_submissions'))
             else:
                 error = 'Invalid login'
                 return render_template('login.html', error=error)
@@ -316,6 +347,125 @@ def logout():
     flash('You are now logged out', 'success')
     return redirect(url_for('login'))
 
+@app.route('/teacher_dashboard')
+@is_logged_in
+def dashboard():
+    result=CURSOR.execute("SELECT notes FROM teacher")## NOTES TABLE
+    notes=CURSOR.fetchall()
+    print( notes)
+    sub_result=CURSOR.execute("SELECT submission FROM teacher")
+    sub=CURSOR.fetchall()
+    return render_template('teacher_dashboard.html',notes=notes,sub=sub)
+
+class Notes(Form):
+    title= StringField('Notes')
+
+@app.route('/add_submission',methods=['GET','POST'])
+@is_logged_in
+def add_submission():
+    form=Notes(request.form)
+    if request.method=='POST':
+        teacher_name=session['username']
+        notes=form.title.data
+        table_name = ""
+        subject_name=("SELECT subject from teacher WHERE login=%s",[teacher_name])
+        if(subject_name==1):
+            table_name = "s1"
+        if(subject_name==2):
+            table_name = "s2"
+        if(subject_name==3):
+            table_name = "s3"
+        CURSOR.execute("UPDATE  %s SET submission=%s",(table_name,notes))
+        CONN.commit()
+        CURSOR.execute("UPDATE  teacher SET submission=%s WHERE login=%s",(notes,teacher_name))
+        CONN.commit()
+        flash("Submission Added",'success')
+        return redirect(url_for('dashboard'))
+    return render_template("add_submission.html",form=form)
+ 
+@app.route('/mark',methods=['GET','POST'])
+@is_logged_in
+def mark():
+    if request.method=='POST':
+        #print request.form.getlist('check_list')
+        return redirect(url_for('dashboard'))
+    #teacher_name=session['username']
+    #subject_name=CURSOR.execute("SELECT subject FROM teacher where login=%s",[teacher_name])
+    #student_details=CURSOR.execute("SELECT student_id FROM %s",(subject_name))
+    else:
+        student_details=CURSOR.execute("SELECT student_id FROM s1")
+        student_details=CURSOR.fetchall()   
+        print( student_details)
+        return render_template('mark.html',student_details=student_details)
+@app.route('/add_notes',methods=['GET','POST'])
+@is_logged_in
+def add_notes():
+    form=Notes(request.form)
+    if request.method=='POST':
+        teacher_name=session['username']
+        #teacher_name="1"
+        notes=form.title.data
+        subject_name=("SELECT subject from teacher WHERE login=%s",[teacher_name])
+        #CURSOR.execute("UPDATE  s%s SET notes=%s",(subject_name,notes))
+        #CONN.commit()
+        CURSOR.execute("UPDATE  teacher SET notes=%s WHERE login=%s",(notes,teacher_name))
+        CONN.commit()
+        flash("Notes Added",'success')
+        return redirect(url_for('dashboard'))
+    return render_template('add_notes.html',form=form)
+class RegisterFromTeacher(Form):
+    login= StringField('Username',[validators.Length(min=1,max=20)])
+    password= PasswordField ('Password',[validators.DataRequired(),validators.EqualTo('confirm',message='password do not match')])
+    confirm=PasswordField('Confirm Password')
+    subject=StringField('Subject',[validators.Length(min=1,max=20)])
+
+@app.route('/teacher_register',methods=['GET','POST'])
+def register_teacher():
+    form =RegisterFrom(request.form)
+    if request.method=='POST' and form.validate():
+        login=form.login.data
+        password=form.password.data
+        subject=form.subject.data
+        # is there something wrong here?
+        #curso
+        #cur=mysql.connection.cursor()
+        CURSOR.execute("INSERT into teacher (login,password,subject) values(%s,%s,%s)",(login,password,subject))
+        #commit
+        CONN.commit()
+        #thats it??
+        #do it wherever you hav conn, cursor
+        #just here
+        #mysql.connection.commit()
+        #close connection
+        #cur.close()
+        #show form html template
+        flash("RESGISTRATION SUCCESSFULL",'success')
+        return redirect(url_for('dashboard'))       
+    return render_template('teacher_register.html',form=form)
+    
+@app.route('/teacher_login',methods=['GET','POST'])
+def login_teacher():
+    if request.method=='POST':
+        login=request.form['login']
+        password_given=request.form['password']
+        result=CURSOR.execute("SELECT * FROM teacher WHERE login=%s",[login])
+        if result>0:
+            data=CURSOR.fetchone() ## gets first row only
+            password=data['password']
+            if(password==password_given):
+                session['logged_in']=True
+                session['username']=login
+                flash('You are successfully logged in','success')
+                return redirect(url_for('dashboard'))
+            else:
+                error='Invalid password'
+                return render_template('teacher_login.html',error=error)
+        else:
+            error='No User Found'
+            return render_template('teacher_login.html',error=error)
+    return render_template('teacher_login.html')
+
+""""
 # Dashboard
 @app.route('/dashboard')
 @is_logged_in
@@ -323,6 +473,7 @@ def dashboard():
     # Create cursor
     cur = mysql.connection.cursor()
     return redirect(url_for('dashboard'))
+"""
 """
     # Get articles
     result = cur.execute("SELECT * FROM articles")
